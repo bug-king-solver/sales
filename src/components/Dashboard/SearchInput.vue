3dc53d603d73c20d6096ae77b36a0de098b78fe7
Michael Govern <michaelgovern0826@gmail.com>
2024-01-10 19:41:01 +0100

    feat: define SearchInput component

---
+++ src/components/Dashboard/SearchInput.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center relative">
+        <SearchIcon class="absolute ml-5 mr-4 pointer-events-none" />
+        <input type="text"
+            class="rounded-[0.1875rem] border border-[#000000] border-opacity-20 pl-[3.75rem] pr-[1.25rem] py-[0.88rem]"
+            placeholder="Search">
+    </div>
+</template>
+
+<script setup lang="ts">
+import SearchIcon from '../icons/SearchIcon.vue';
+</script>
