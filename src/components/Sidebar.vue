5270525c999dd00b1fdc997c2145bd47e16e7881
Michael Govern <michaelgovern0826@gmail.com>
2024-01-12 08:01:05 +0100

    fix: update active link

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,50 +4,58 @@
             <p class="text-[#013CC6] font-poppins text-2xl font-medium">Sales.</p>
         </div>
         <div class="flex flex-col gap-[2.56rem] mt-[3.62rem]">
-            <router-link to="/" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]">
-                <DashboardIcon />
+            <router-link to="/" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]"
+                :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/' }">
+                <DashboardIcon :isActive="$route.path === '/'" />
                 <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer">
                     Dashboard
                 </label>
             </router-link>
-            <router-link to="/lab-test" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]">
+            <router-link to="/lab-test" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]"
+                :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/lab-test' }">
                 <LabTestIcon />
                 <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer">
                     Lab Test
                 </label>
             </router-link>
-            <router-link to="/appointment" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]">
+            <router-link to="/appointment" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]"
+                :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/appointment' }">
                 <AppointmentIcon />
                 <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer">
                     Appointment
                 </label>
             </router-link>
-            <router-link to="/medicine-order" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]">
+            <router-link to="/medicine-order" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]"
+                :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/medicine-order' }">
                 <MedicineOrderIcon />
                 <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer whitespace-nowrap">
                     Medicine Order
                 </label>
             </router-link>
-            <router-link to="/message" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]">
+            <router-link to="/message" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]"
+                :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/message' }">
                 <MessageIcon />
                 <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer">
                     Message
                 </label>
             </router-link>
-            <router-link to="/payment" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]">
+            <router-link to="/payment" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]"
+                :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/payment' }">
                 <PaymentIcon />
                 <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer">
                     Payment
                 </label>
             </router-link>
-            <router-link to="/settings" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]">
+            <router-link to="/settings" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem]"
+                :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/settings' }">
                 <SettingsIcon />
                 <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer">
                     Settings
                 </label>
             </router-link>
         </div>
-        <router-link to="/help" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem] mt-auto">
+        <router-link to="/help" class="flex gap-4 pt-[0.44rem] pb-[0.5rem] pl-[2.56rem] mt-auto"
+            :class="{ 'border-l-4 border-[#0B63F8] text-[#0B63F8]': $route.path === '/help' }">
             <HelpIcon />
             <label class="font-poppins text-base font-normal tracking-[0.02rem] cursor-pointer">Help</label>
         </router-link>
